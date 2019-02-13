378fe35dc4592aece0b726dc823feeb07b1ceda5
AG <32141008+agacayan@users.noreply.github.com>
2019-02-13 11:56:49 -0800

    Add files via upload

---
+++ hide password.ps1
@@ -0,0 +1,13 @@
+﻿
+#Creating encrypted password file
+Read-Host -AsSecureString | ConvertFrom-SecureString | Out-File -FilePath <filename>.securestring
+
+#Decrypting password file
+$PlainPassword = Get-Content C:\Users\youruser\Documents\<filename>.securestring
+
+$SecurePassword = ConvertTo-SecureString $PlainPassword
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($SecurePassword)
+$UnsecurePassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto($BSTR)
+
+$UnsecurePassword | Out-GridView
+
